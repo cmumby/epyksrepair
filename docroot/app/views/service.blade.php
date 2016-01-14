28851b472f35abc6dbb639394fcf26233a8ebc8f
Curtis Mumby <curtismumby@Curtiss-MacBook-Pro-2.local>
2016-01-14 17:06:47 -0500

    Shortend Placeholder text on Services Page

--- docroot/app/views/service.blade.php
+++ docroot/app/views/service.blade.php
@@ -4,7 +4,7 @@
    <div class="span6">
       <img src="{{URL::asset('/images/repair/service-web.jpg')}}">
       <h2>Ready for Repair?</h2>
-      <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+      <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. </p>
    </div>
    <div class="span6">
       <h2 class="form-title">Find the best Tech for your Appliance!</h2>
